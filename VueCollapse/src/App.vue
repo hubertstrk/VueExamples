8bcc816ed76ef36e0b13ef07020304891fcfa5e0
Hubert Stärk <hubert.staerk@gmail.com>
2017-10-09 23:03:04 +0200

    Inital checkin VueCollapse

---
+++ VueCollapse/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <hello-world/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+</style>
