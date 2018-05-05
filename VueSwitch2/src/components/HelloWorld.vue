cfce45c43b91d112f4cb09e83650df3f801f3cb2
Hubert Stärk <hubert.staerk@gmail.com>
2018-05-05 21:56:11 +0200

    vue switch2 color, size

--- VueSwitch2/src/components/HelloWorld.vue
+++ VueSwitch2/src/components/HelloWorld.vue
@@ -1,7 +1,11 @@
 <template>
   <div>
     <h1>{{ msg }}</h1>
-    <vue-switch v-model="checked" small></vue-switch>
+    <vue-switch v-model="checked" text="Foo" success></vue-switch>
+    <br>
+    <vue-switch v-model="checked" text="Bar" small danger></vue-switch>
+    <br>
+    <vue-switch v-model="checked" text="Bar" small></vue-switch>
     <p>{{checked}}</p>
   </div>
 </template>
