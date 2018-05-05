0155c6589f5a06cb7103b87b0e6b4275998166a2
Hubert Stärk <hubert.staerk@gmail.com>
2018-05-05 21:09:54 +0200

    vue switch2 inital check in

---
+++ VueSwitch2/src/components/HelloWorld.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+    <vue-switch v-model="checked" small></vue-switch>
+    <p>{{checked}}</p>
+  </div>
+</template>
+
+<script>
+import VueSwitch from './VueSwitch.vue'
+
+export default {
+  props: {
+    msg: String
+  },
+  data () {
+    return {
+      checked: true
+    }
+  },
+  components: {VueSwitch}
+}
+</script>
+
+<style lang="css" scoped>
+
+</style>
---
+++ VueSwitch2/src/components/VueSwitch.vue
@@ -0,0 +1,56 @@
+<template>
+  <label class="switch">
+    <input v-model="checked" type="checkbox" />
+    <span :class="[{ translate: checked }]" class="slider"></span>
+  </label>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        checked: false,
+      }
+    },
+    props: {
+      value: Boolean,
+      small: Boolean
+    },
+    methods: {
+    },
+    watch: {
+      checked (value) {
+        this.$emit('input', value)
+        
+      }
+    },
+    mounted () {
+      this.checked = this.value
+    }
+  }
+</script>
+
+<style lang="css" scoped>
+.switch {
+  display: flex;
+  align-items: center;
+  height: 22px;
+  width: 38px;
+  border-radius: 12px;
+  background-color: #ccc;
+}
+.slider {
+  height: 16px;
+  width: 16px;
+  margin-left: 3px;
+  border-radius: 16px;
+  background-color: rgb(255, 255, 255);
+  transition: .4s;
+}
+.translate {
+  transform: translateX(16px);
+}
+input {
+  display: none;
+}
+</style>
