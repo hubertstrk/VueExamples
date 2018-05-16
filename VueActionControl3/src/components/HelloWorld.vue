fb1c5aed64b177660f05da1c5d366a77c0094ef4
Hubert Stärk <hubert.staerk@gmail.com>
2018-05-17 00:17:43 +0200

    VueActionControl3 animation

--- VueActionControl3/src/components/HelloWorld.vue
+++ VueActionControl3/src/components/HelloWorld.vue
@@ -1,26 +1,31 @@
 <template>
-<div class="container">
-  <div class="fab" :class="collapsed ? 'collapsed' : 'expanded'">
-    <div class="fab-circle" @click="collapsed = !collapsed">
-      <i :class="collapsed ? 'fas fa-expand-arrows-alt' : 'fas fa-arrows-alt'"></i>
-    </div>
-    <transition-group name="fade">
-      <template v-if="!collapsed">
-        <div key="tl" class="action action-tl">
-          1
-        </div>
-        <div key="tr" class="action action-tr">
-          2
-        </div>
-        <div key="bl" class="action-small action-bl">
-          3
-        </div>
-        <div key="br" class="action-small action-br">
-          4
-        </div>
-      </template>
-    </transition-group>
+
+<div class="fab" :class="fabCollapsed ? 'collapsed' : 'expanded'">
+  <div class="fab-circle" @click="fabCollapsed = !fabCollapsed">
+    <i :class="fabCollapsed ? 'fas fa-expand-arrows-alt' : 'fas fa-arrows-alt'"></i>
   </div>
+  <transition-group name="fade">
+    <template v-if="!fabCollapsed">
+      <div key="tl" class="box box-medium box-tl">
+        1
+      </div>
+      <div key="tr" class="box box-medium box-tr">
+        2
+      </div>
+      <div key="bl" class="box box-small box-bl" :class="{'bl-expanded': blExpanded}">
+        <a href="#" @click.prevent="blExpanded = !blExpanded">Expand</a>
+        <div v-if="blExpanded" class="bl-content">
+          <a href="#">Some Text</a><br>
+        </div>
+      </div>
+      <div key="br" class="box box-small box-br" :class="{'br-expanded': brExpanded}">
+        <a href="#" @click.prevent="brExpanded = !brExpanded">Expand</a>
+        <div v-if="brExpanded" class="br-content">
+          <a href="#">Some Text</a><br>
+        </div>
+      </div>
+    </template>
+  </transition-group>
 </div>
 
 </template>
@@ -31,7 +36,9 @@ export default {
   },
   data () {
     return {
-      collapsed: false
+      fabCollapsed: false,
+      brExpanded: false,
+      blExpanded: false
     }
   }
 }
@@ -50,6 +57,8 @@ export default {
   height: 60px;
   width: 320px;
 }
+
+/* action boxes fade effect */
 .fade-enter-active, .fade-leave-active {
   transition: opacity .4s;
 }
@@ -60,10 +69,9 @@ export default {
 .fab {
   font-size: 12px;
   position: absolute;
-  /* height: 60px;
-  width: 320px; */
 }
-.fab-circle { 
+.fab-circle {
+  /* circle styling */
   position: absolute;
   top: 6px;
   left: 138px;
@@ -71,48 +79,68 @@ export default {
   width: 45px;
   border-radius: 45px;
   border: 1px solid #ddd;
-  z-index: 100;
   background-color: white;
-  display: flex;
+  z-index: 100;
+  /* icon positioning */
+  display: flex; 
   align-items: center;
   justify-content: center;
-  transition: all .4s ease;
+  /* transition for hover effect */
+  transition: all .4s ease; 
 }
 .fab-circle:hover {
   background-color: rgb(236, 236, 236);
 }
 
-.action {
+.box {
+  position: absolute;
+  padding: 5px;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+}
+.box-medium {
   height: 25px;
   width: 145px;
   border-radius: 5px;
   border: 1px solid #ddd;
 }
-.action-small {
+.box-small {
   height: 25px;
   width: 125px;
   border-radius: 5px;
   border: 1px solid #ddd;
 }
-.action-tl {
-  position: absolute;
+.box-tl {
   top: 0px;
   left: 0px;
 }
-.action-tr {
-  position: absolute;
+.box-tr {
   top: 0px;
   right: 0px;
 }
-.action-bl {
-  position: absolute;
+.box-bl {
   top: 35px;
   left: 20px;
-
+  transition: all .4s ease;
 }
-.action-br {
-  position: absolute;
+.box-br {
   top: 35px;
   right: 20px;
+  transition: all .4s ease;
+}
+
+.br-expanded {
+  height: 100px;
+}
+.br-content {
+  margin-top: 10px;
+}
+
+.bl-expanded {
+  height: 50px;
+}
+.bl-content {
+  margin-top: 10px;
 }
 </style>
